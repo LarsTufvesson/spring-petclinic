29ab1e82c639759cadc480f63599492e4d6dfdbc
Debian <admin@ip-172-31-28-3.eu-west-1.compute.internal>
2019-11-17 10:24:21 +0000

    Updated URL for Assertible

--- curl_assert.sh
+++ curl_assert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -XPOST https://assertible.com/apis/40a35bd7-dece-4cf4-913d-30f8a718efd5/run?api_token=70yrVBaucYe3KCyD -d'{
+curl -XPOST https://assertible.com/apis/9fa64037-edb3-4758-b441-682ec994b113/run?api_token=KxEovfqQK2S5Zcxe -d'{
 	"wait": true
 }'
